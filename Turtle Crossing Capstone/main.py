ac4c0c156cdf4d12ed08b873ba6b87ed89076da0
pthh2 <110741295+pthh2@users.noreply.github.com>
2025-05-07 12:08:01 -0500

    Add files via upload

---
+++ Turtle Crossing Capstone/main.py
@@ -0,0 +1,47 @@
+'''
+Description: This is a game project. A turtle is controlled by the player from
+the computer keyboard to cross the road while a series of cars are running from
+right to left on that road. The turtle hits the car and the game is over.
+If it can get to the other side of the road, it will increase its level on
+the board. Each time the board increases in level, the speed of the cars will
+increase.
+'''
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #dectect seccessful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
+
